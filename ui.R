305a340739f6134c0d765ab17bf4b62a10eed781
bgulbis <brian.gulbis@gmail.com>
2016-12-11 19:57:39 -0600

    update title

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Text Prediction App"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
